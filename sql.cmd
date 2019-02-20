5e8dd39190654b3602814278500a87b120d6e62f
pawelfab <46033378+pawelfab@users.noreply.github.com>
2019-02-20 16:15:20 +0100

    Update sql.cmd

--- sql.cmd
+++ sql.cmd
@@ -1,10 +1,21 @@
-select 
-MONTH(Dok.Data) as miesiac,YEAR(Dok.Data) as rok,SUM(Dok.Razem) as Razem, Magazyn.Nazwa as Magazyn, count(Dok.NrDok) as iloscParagon, 
-(SUM(Dok.Razem)/count(Dok.NrDok)) as SredParagonu
-from 
-Dok INNER JOIN Magazyn ON Dok.MagId=Magazyn.MagId 
-where 
-TypDok=21 
-GROUP BY 
-YEAR(Dok.Data),MONTH(Dok.Data), Magazyn.Nazwa order by YEAR(Dok.Data) 
-desc
+DECLARE @StartDate DATETIME
+DECLARE @EndDate DATETIME
+DECLARE @WorkingDays int
+DECLARE @DayTillToday int
+SET @StartDate = (SELECT DATEADD(month, DATEDIFF(month, 0, GETDATE()), 0))
+SET @EndDate = (SELECT DATEADD(month, ((YEAR(GETDATE()) - 1900) * 12) + MONTH(GETDATE()), -1))
+
+
+set @WorkingDays = (SELECT
+   (DATEDIFF(dd, @StartDate, @EndDate) + 1)
+  -(DATEDIFF(wk, @StartDate, @EndDate) * 2)
+  -(CASE WHEN DATENAME(dw, @StartDate) = 'Sunday' THEN 1 ELSE 0 END)
+  -(CASE WHEN DATENAME(dw, @EndDate) = 'Saturday' THEN 1 ELSE 0 END)+2)
+  
+ set @DayTillToday = (SELECT
+	(DATEDIFF(dd, @StartDate, GETDATE()) + 1)
+  -(DATEDIFF(wk, @StartDate, GETDATE()) * 2)
+  -(CASE WHEN DATENAME(dw, @StartDate) = 'Sunday' THEN 1 ELSE 0 END)
+  -(CASE WHEN DATENAME(dw, GETDATE()) = 'Saturday' THEN 1 ELSE 0 END)+2)
+  
+select (select SUM(Dok.Razem)from Dok where TypDok=21 and Month(data) = Month(CAST(GETDATE() AS date)) and YEAR(data) = YEAR(CAST(GETDATE() AS date)) and MagId=1)/@DayTillToday * @WorkingDays as estymacja
